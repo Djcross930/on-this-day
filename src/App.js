2e02156c344d17effac6a34ebf5087d9582aaf27
Dan Cross <dancross96@gmail.com>
2022-10-12 17:30:27 -0500

    added dynamic api, the website will now always display the current day

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import React from 'react'
 function App() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0');
   const [mainData, setMainData] = React.useState([])
   React.useEffect(() => {
     async function getStuff() {
-      const res = await fetch("https://byabbe.se/on-this-day/10/4/events.json")
+      const res = await fetch(`https://byabbe.se/on-this-day/${mm}/${dd}/events.json`)
       const data = await res.json()
       setMainData(data.events)
     }
     getStuff()
   }, [])
-  React.useEffect(() => {
-    console.log(mainData)
-  }, [])
-  console.log(mainData)
+
 
   return <div>
     {mainData.map(item => <div key={item.description}><p> <b>Year:</b> {item.year} </p> <p> <b>Event:</b> {item.description} </p> </div>)}
