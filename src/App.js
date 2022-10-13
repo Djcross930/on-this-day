12afe50de790ebcff0a219802122786a10314043
Dan Cross <dancross96@gmail.com>
2022-10-12 19:07:17 -0500

    added nav bar and changed some colors, deff want to work on the css more.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import './App.css';
 import React from 'react'
+import Nav from './components/Nav.js'
 function App() {
-  var today = new Date();
-  var dd = String(today.getDate()).padStart(2, '0');
-  var mm = String(today.getMonth() + 1).padStart(2, '0');
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+    "July", "August", "September", "October", "November", "December"
+  ];
+  const today = new Date();
+  const dd = String(today.getDate()).padStart(2, '0');
+  const mm = String(today.getMonth() + 1).padStart(2, '0');
+  const month = monthNames[(mm - 1)]
   const [mainData, setMainData] = React.useState([])
   React.useEffect(() => {
-    async function getStuff() {
+    async function getEvents() {
       const res = await fetch(`https://byabbe.se/on-this-day/${mm}/${dd}/events.json`)
       const data = await res.json()
       setMainData(data.events)
     }
-    getStuff()
+    getEvents()
   }, [])
 
 
   return <div>
-    {mainData.map(item => <div key={item.description}><p> <b>Year:</b> {item.year} </p> <p> <b>Event:</b> {item.description} </p> </div>)}
+    <Nav />
+    <div className="mainBody">
+      <h2 className="theDate"> {month} {dd} </h2>
+      {mainData.map(item => <div className="main" key={item.description}><p> <b>Year:</b> {item.year} </p> <p> <b>Event:</b> {item.description} </p> </div>)}
+    </div>
   </div>
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import Roman from './Roman.png'
+export default function Nav() {
+  return <div className="NavBar"><img className="navImage" src={Roman} alt="The Course of Empire(1836)" /> <h2 className="navText">On This Day...</h2>  </div>
+}
